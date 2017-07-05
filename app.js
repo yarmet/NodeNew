83873b7f70f0e8c2a13133c8436373c53c185294
ruslan <yarmet84@mail.ru>
2017-07-05 22:54:25 +0300

    refactoring

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css'))); // redirect CSS bootstrap
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
