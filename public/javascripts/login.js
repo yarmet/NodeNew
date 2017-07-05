626cfb4d836be8ccd08d205d5f93053196e11831
ruslan <yarmet84@mail.ru>
2017-07-05 23:56:29 +0300

    refactoring

---
+++ public/javascripts/login.js
@@ -0,0 +1,97 @@
+/**
+ * Created by ruslan on 05.07.2017.
+ */
+
+
+class Input extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {border: "red", isValid: false};
+        this.change = this.change.bind(this);
+    }
+
+    change(e) {
+        var txt = e.target.value;
+        if (txt.length < 3) {
+            this.state = {border: "red", isValid: false, value: txt};
+        } else {
+            this.state = {border: "green", isValid: true, value: txt};
+        }
+        this.props.change();
+    }
+
+    render() {
+        return React.createElement('input', {
+            className: "form-control",
+            onChange: this.change,
+            type: this.props.type,
+            style: {borderColor: this.state.border}
+        }, null);
+    }
+}
+
+
+class Form extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {buttonDisabled: true};
+        this.inputChange = this.inputChange.bind(this);
+        this.send = this.send.bind(this);
+    }
+
+    inputChange() {
+        if (this.refs.input1.state.isValid && this.refs.input2.state.isValid) {
+            this.setState({buttonDisabled: false})
+        } else {
+            this.setState({buttonDisabled: true})
+        }
+    }
+
+    send() {
+        console.log(this.refs.input1.state.value + " " + this.refs.input2.state.value)
+    }
+
+    render() {
+        return (
+            React.createElement('div', null,
+                React.createElement('div', {className: "form-group"},
+                    React.createElement(Input, {type: "text", ref: "input1", change: this.inputChange}, null)),
+
+                React.createElement('div', {className: "form-group"},
+                    React.createElement(Input, {type: "password", ref: "input2", change: this.inputChange}, null)),
+
+                React.createElement('div', {className: "form-group"},
+                    React.createElement("label", {htmlFor: "rem"}, "запомнить"),
+                    React.createElement("input", {type: "checkbox", id: "rem"}, null)),
+
+                React.createElement('div', {className: "form-group"},
+                    React.createElement('button', {
+                        className: 'btn btn-success',
+                        disabled: this.state.buttonDisabled,
+                        onClick: this.send
+                    }, 'отправить')),
+
+                React.createElement('a', {href: "/registry"}, "зарегистрироваться")
+            )
+        )
+
+    }
+}
+
+
+// form(method="post" action="/login" )
+// div.form-group
+// input.form-control(type="username",name="username", placeholder="логин")
+// div.form-group
+// input.form-control(type="password",name="password", placeholder="пароль")
+// div.checkbox
+// label
+// input(type="checkbox",name="remember")
+// span запомнить
+// button#login.btn.btn-success войти
+
+ReactDOM.render(
+    React.createElement(Form, null, null),
+    document.getElementById('react')
+);
