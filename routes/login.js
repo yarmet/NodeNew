498fd4caa225f009693d7324e01a262ba34841cf
ruslan <yarmet84@mail.ru>
2017-07-25 00:08:02 +0300

    deleted react

--- routes/login.js
+++ routes/login.js
@@ -12,16 +12,18 @@ exports.get = function (req, res) {
 exports.post = function (req, res, next) {
     var username = req.body.username;
     var password = req.body.password;
+
     var remember = req.body.remember;
+
 
     User.findOne({username: username}, function (err, user) {
         if (err)  return next(err);
         if (user && user.checkpassword(password)) {
             req.session.user = user.username;
-            if (remember === true) {
+            if(remember ==="on"){
                 req.session.cookie.maxAge = 604800000; // one week
             }
-            res.render('index', {title: 'Express', user: user.username});
+            res.render('index', { title: 'Express', user:  user.username });
         } else {
             return next(new HttpError(403, "не правильный логин или пароль"));
         }
